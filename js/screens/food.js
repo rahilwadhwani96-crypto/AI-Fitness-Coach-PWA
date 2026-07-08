10384da13647a3f49eb6d13a9d4631ede2557d71
rahilwadhwani96-crypto <rahilwadhwani96@gmail.com>
2026-07-08 10:37:25 +0530

    Create food.js

---
+++ js/screens/food.js
@@ -0,0 +1,8 @@
+export function renderFood(container) {
+  container.innerHTML = `
+    <h1 class="screen-title">Food</h1>
+    <section class="card">
+      <p class="hint">Photo-based meal analysis arrives in a later milestone.</p>
+    </section>
+  `;
+}
