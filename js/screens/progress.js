dc053cc59097ddfa3ef9e3718665929734036752
rahilwadhwani96-crypto <rahilwadhwani96@gmail.com>
2026-07-08 10:36:54 +0530

    Create progress.js

---
+++ js/screens/progress.js
@@ -0,0 +1,8 @@
+export function renderProgress(container) {
+  container.innerHTML = `
+    <h1 class="screen-title">Progress</h1>
+    <section class="card">
+      <p class="hint">Your weight graph and goal tracking arrive in a later milestone.</p>
+    </section>
+  `;
+}
