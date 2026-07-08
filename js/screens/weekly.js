a432ce6a02a3c2522764398be86975fb3423cd05
rahilwadhwani96-crypto <rahilwadhwani96@gmail.com>
2026-07-08 10:36:27 +0530

    Create weekly.js

---
+++ js/screens/weekly.js
@@ -0,0 +1,8 @@
+export function renderWeekly(container) {
+  container.innerHTML = `
+    <h1 class="screen-title">Weekly</h1>
+    <section class="card">
+      <p class="hint">Weekly completion, duration, and calorie stats arrive in a later milestone.</p>
+    </section>
+  `;
+}
