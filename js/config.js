e42cd58f6f66e0447d3d3e7b03c5f218efc517ed
rahilwadhwani96-crypto <rahilwadhwani96@gmail.com>
2026-07-09 21:13:13 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -12,6 +12,6 @@
  * the same note.
  */
 export const CONFIG = {
-  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbyK7wG_kmYOKtiIMBuxoQpwQaqXW6fd2rgM1y3UAHoakHRvJhwg75S6u512LN5iaomifA/exec',
+  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbzFFYXmEJke2QPVGCkvlG5XQym9uwwF3OrymPIFWyJbjxMLhy3rpmjC3Tkw81RWusXudA/exec',
   API_TOKEN: 'fitcoach-9x7k2m4p8q1w5z3z',
 };
